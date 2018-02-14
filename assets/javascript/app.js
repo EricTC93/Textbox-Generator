4f9eee64385da5caa4d23fc88b000001406276d7
YETC7 <tyler_726@live.com>
2018-02-14 05:38:47 -0800

    displays text to screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,27 @@
+// $("#textinput").val("")
+
 $("#playButton").on("click",function(event) {
 	event.preventDefault();
 
 	let txt = $("#textinput").val().trim();
+
+	let txtArr = txt.split("");
+
+	displayText(txtArr,"");
 	
-	$("#textdisplay").val(txt);
+	// $("#textdisplay").val(txt);
+
 });
+
+function displayText(arr,str) {
+
+	if (arr.join("") === str) {
+		return;
+	}
+
+	let i = str.length;
+	let char = arr[i];
+	str = str + char;
+	$("#textdisplay").val(str);
+	setTimeout(function() {displayText(arr,str);},40);
+}
